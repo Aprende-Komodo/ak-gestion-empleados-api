b57e2f5dd86f4883ade3efd187499e2e8f593963
Luis Briceno <luijo265@gmail.com>
2021-09-18 09:05:26 -0500

    Seccion 6 finalizada

--- infraestructure/repositories/connections.js
+++ infraestructure/repositories/connections.js
@@ -1,13 +1,13 @@
 const db = require('knex')({
-    client: 'mysql',
-    connection: {
-      host : 'compania.c3bnfogag1zx.us-east-1.rds.amazonaws.com',
-      port : 3306,
-      user : 'admin_dev',
-      password : '12345678*',
-      database : 'empleados_dev'
-    }
-  });
+  client: 'mysql',
+  connection: {
+    host : process.env.MYSQL_HOST,
+    port : process.env.MYSQL_PORT,
+    user : process.env.MYSQL_USER,
+    password : process.env.MYSQL_PW,
+    database : process.env.MYSQL_DB
+  }
+});
 
 module.exports = {
     db
